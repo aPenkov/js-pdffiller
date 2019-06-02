abf7c55cc7d0d2cea60f8355012449f498149ca0
sasha <alexsandr.penkov@drimsim.com>
2019-06-02 22:34:22 +0300

    fixed chunk function for not full arrays in result

--- homework-week1.js/homework-week-1.js
+++ homework-week1.js/homework-week-1.js
@@ -50,20 +50,39 @@ function uniq(array) {
 function chunk(array, size) {
 	const resultArray = [];
 	let arrSizeCount = size;
+	
 	array.reduce((acc, el, i) => {
   		if (i < arrSizeCount) {
   			acc.push(el);
-  			if (i === array.length - 1) {
-  				resultArray.push(acc);
-
-  			}
-  			return acc;
+			if (i === (array.length - 1)) {
+				resultArray.push(acc);
+			}
+  			return acc;  			
   		}
+
   		resultArray.push(acc);
   		acc = [];
   		acc.push(el);
+
+	  	if (i === (array.length - 1)) {
+			resultArray.push(acc);
+		}
+		
   		arrSizeCount = size*2;
-  		return acc;
+  		return acc; 
 	}, [])
+
 	return resultArray;
-}
+} //  не получилось избавится от повторяющихся блоков if 
+
+console.log(difference([2, 1, 5], [2, 3]))
+
+console.log(groupBy([{ gender: 'male', name: 'Max'}, 
+	{ gender: 'male', name: 'Fred'}, 
+	{ gender: 'female', name: 'Jane'}], 'gender'))
+
+console.log(flatten([1, [2, [3, [4]], 5]]))
+
+console.log(uniq([2, 1, 2]))
+
+console.log(chunk(['a', 'b', 'c', 'd','e'], 4))
